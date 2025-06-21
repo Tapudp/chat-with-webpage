341db61c3d6743c43e4a459901ede7e0db1a6366
Tapudp <divyeshpar2496@gmail.com>
2025-06-21 22:46:49 +0530

    chore: setup base files with pnpm

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react"],
+  root: true,
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
